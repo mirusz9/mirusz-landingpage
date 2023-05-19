38f24663b3d25a83e0e0481365dedf693f7db78a
mirusz9 <adam.sztano@gmail.com>
2023-05-19 21:03:11 +0200

    forcing port

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const buildFolderPath = path.join(__dirname, '..', '..', 'dist');
 const app = express();
-const PORT = +(process?.env.PORT || 3000);
+// const PORT = +(process?.env.PORT || 3000);
+const PORT = 3000;
 
 
 app.use(helmet());
